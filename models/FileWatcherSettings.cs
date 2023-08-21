2f1ce7894cad6e43e881f9ad503733d45fdb23dd
Jon Rumsey <jon.rumsey@gmail.com>
2023-08-20 17:44:16 -0700

    Created FileWatcherSettings wrapper class.

---
+++ models/FileWatcherSettings.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace file_sync_win.models
+{
+    internal class FileWatcherSettings
+    {
+        public string FilePath { get; set; }
+        public string FileType { get; set; }
+        public string ServerAddress { get; set; }
+
+        public FileWatcherSettings() { }
+
+        public FileWatcherSettings(string filePath, string fileType, string serverAddress)
+        {
+            FilePath = filePath;
+            FileType = fileType;
+            ServerAddress = serverAddress;
+        }
+
+        public void GetSettings()
+        {
+            FilePath = Environment.GetEnvironmentVariable("FSW_FILEPATH");
+            FileType = Environment.GetEnvironmentVariable("FSW_FILETYPE");
+            ServerAddress = Environment.GetEnvironmentVariable("FSW_SERVER_ADDRESS");
+        }
+
+        public override string ToString()
+        {
+            return $"Server: {ServerAddress} FilePath: {FilePath} Filter: {FileType}";
+        }
+    }
+}

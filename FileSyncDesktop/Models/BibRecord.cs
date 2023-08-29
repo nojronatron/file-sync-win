a6a6c805e377d442212a8fec1b12afc8b483b562
Jon Rumsey <jon.rumsey@gmail.com>
2023-08-29 09:18:20 -0700

    tweaked bibrecord class.

--- FileSyncDesktop/Models/BibRecord.cs
+++ FileSyncDesktop/Models/BibRecord.cs
@@ -8,23 +8,19 @@ namespace FileSyncDesktop.Models
 {
     public class BibRecord
     {
-        public int Id { get; set; }
         public int BibNumber { get; set; }
         public string Action { get; set; }
         public string BibTimeOfDay { get; set; }
         public int DayOfMonth { get; set; }
-        public string Location { get; set; }
         public string ShortLocation { get; set; }
 
         public BibRecord() { }
-        public BibRecord(int id, int bibNumber, string action, string bibTimeOfDay, int dayOfMonth, string location, string shortLocation)
+        public BibRecord(int bibNumber, string action, string bibTimeOfDay, int dayOfMonth, string shortLocation)
         {
-            Id = id;
             BibNumber = bibNumber;
             Action = action;
             BibTimeOfDay = bibTimeOfDay;
             DayOfMonth = dayOfMonth;
-            Location = location;
             ShortLocation = shortLocation;
         }
     }

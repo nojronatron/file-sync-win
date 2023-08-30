5eb1af5d26e0d5bf2ea5fe639d622edf7d5a44fe
Jon Rumsey <jon.rumsey@gmail.com>
2023-08-29 21:41:50 -0700

    Implemented FileSyncDesktop.Library.

---
+++ FileSyncDesktop.Library/Api/BibReportEndpoint.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using FileSyncDesktop.Library.Helpers;
+using FileSyncDesktop.Library.Models;
+
+namespace FileSyncDesktop.Library.Api
+{
+    public class BibReportEndpoint : IBibReportEndpoint
+    {
+        private IAPIHelper _apiHelper;
+
+        public BibReportEndpoint(IAPIHelper apiHelper)
+        {
+            _apiHelper = apiHelper;
+        }
+
+        public async Task PostBibReport(BibRecords bibRecords)
+        {
+            var requestUri = "/api/BibRecords";
+            var httpContent = new StringContent(bibRecords.ToJson(), Encoding.UTF8, "application/json");
+
+            using (HttpResponseMessage response = await _apiHelper.ApiClient.PostAsync(requestUri, httpContent))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    // todo: log success
+                    Console.WriteLine("Success");
+                }
+                else
+                {
+                    // todo: log failure
+                    Console.WriteLine(response.ReasonPhrase);
+                }
+            }
+        }
+    }
+}
---
+++ FileSyncDesktop.Library/Models/BibRecordModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileSyncDesktop.Library.Models
+{
+    public class BibRecordModel
+    {
+        public int BibNumber { get; set; }
+        public string Action { get; set; }
+        public string BibTimeOfDay { get; set; }
+        public int DayOfMonth { get; set; }
+        public string ShortLocation { get; set; }
+    }
+}
